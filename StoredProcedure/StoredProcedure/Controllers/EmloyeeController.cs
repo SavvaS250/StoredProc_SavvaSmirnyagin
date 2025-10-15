901c6632326d68f76a3193d890bb7c3adc45494d
Opilane TTHK <opilane@tthk.ee>
2025-10-15 14:12:09 +0300

    veel üks stored procedure

--- StoredProcedure/StoredProcedure/Controllers/EmloyeeController.cs
+++ StoredProcedure/StoredProcedure/Controllers/EmloyeeController.cs
@@ -103,8 +103,92 @@ namespace StoredProcedure.Controllers
 
                 return View(model);
             }
-        } 
+        }
 
+        [HttpGet]
+        public IActionResult SearchDynamicSQL()
+        {
+            string connectionStr = _config.GetConnectionString("DefaultConnection");
+
+            using (SqlConnection con = new SqlConnection(connectionStr))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = "dbo.spSearchEmployeesGoodDynamicSQL";
+                cmd.CommandType = System.Data.CommandType.Text;
+                con.Open();
+                SqlDataReader sdr = cmd.ExecuteReader();
+                List<Employee> model = new List<Employee>();
+                while (sdr.Read())
+                {
+                    var details = new Employee();
+                    details.FirstName = sdr["FirstName"].ToString();
+                    details.LastName = sdr["LastName"].ToString();
+                    details.Gender = sdr["Gender"].ToString();
+                    details.Salary = Convert.ToInt32(sdr["Salary"]);
+                    model.Add(details);
+                }
+
+                return View(model);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult SearchWithDynamicSQL(string firstName, string lastName, string gender, int salary)
+        {
+            string connectionStr = _config.GetConnectionString("DefaultConnection");
+            using (SqlConnection con = new SqlConnection(connectionStr))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                StringBuilder sbCommand = new StringBuilder("SELECT * FROM Employees WHERE 1=1");
+
+                if (!string.IsNullOrEmpty(firstName))
+                {
+                    sbCommand.Append(" AND FirstName=@FirstName");
+                    SqlParameter parameter = new SqlParameter("@FirstName", firstName);
+                    cmd.Parameters.Add(parameter);
+                }
+
+                if (!string.IsNullOrEmpty(lastName))
+                {
+                    sbCommand.Append(" AND LastName=@LastName");
+                    SqlParameter parameter = new SqlParameter("@LastName", lastName);
+                    cmd.Parameters.Add(parameter);
+                }
+
+                if (!string.IsNullOrEmpty(gender))
+                {
+                    sbCommand.Append(" AND Gender=@Gender");
+                    SqlParameter parameter = new SqlParameter("@Gender", gender);
+                    cmd.Parameters.Add(parameter);
+                }
+
+                if (salary != 0)
+                {
+                    sbCommand.Append(" AND Salary=@Salary");
+                    SqlParameter parameter = new SqlParameter("@Salary", salary);
+                    cmd.Parameters.Add(parameter);
+                }
+                cmd.CommandText = sbCommand.ToString();
+                cmd.CommandType = System.Data.CommandType.Text;
+                con.Open();
+                SqlDataReader sdr = cmd.ExecuteReader();
+                List<Employee> model = new List<Employee>();
+
+                while (sdr.Read())
+                {
+                    var details = new Employee();
+                    details.FirstName = sdr["FirstName"].ToString();
+                    details.LastName = sdr["LastName"].ToString();
+                    details.Gender = sdr["Gender"].ToString();
+                    details.Salary = Convert.ToInt32(sdr["Salary"]);
+                    model.Add(details);
+                }
+
+                return View(model);
+            }
+        }
 
     }
 }
